b1057a3328514dbcbbda3d8a7051ced2c1605741
kgy1008 <gayeon1008@g.skku.edu>
2025-07-19 15:46:23 +0900

    [feat] implement multi-module structure for Edukit project

---
+++ edukit-api/src/main/java/com/edukit/api/EdukitApiApplication.java
@@ -0,0 +1,13 @@
+package com.edukit.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdukitApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdukitApiApplication.class, args);
+    }
+
+} 
---
+++ edukit-batch/src/main/java/com/edukit/batch/EdukitBatchApplication.java
@@ -0,0 +1,13 @@
+package com.edukit.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdukitBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdukitBatchApplication.class, args);
+    }
+
+} 
