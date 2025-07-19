c36583e05db846b2b87aa48e3b00d088bafa3ff8
kgy1008 <gayeon1008@g.skku.edu>
2025-07-19 15:39:24 +0900

    [refac] refactor project structure and add initial application files

---
+++ src/test/java/com/edukit/eduserver/EduserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edukit.eduserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+} 
