980cf947961b1f530b410f7c6b4952312ba5bac6
kgy1008 <gayeon1008@g.skku.edu>
2025-07-19 21:58:23 +0900

    [fix] correct parameter order in ApiResponse constructors and methods

--- edukit-api/src/main/java/com/edukit/api/common/ApiResponse.java
+++ edukit-api/src/main/java/com/edukit/api/common/ApiResponse.java
@@ -14,33 +14,33 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class ApiResponse<T> {
 
+    @JsonIgnore
+    private final int status;
+
     private final String code;
 
     private final String message;
-
-    @JsonIgnore
-    private final int status;
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private T data;
 
     public static <T> ApiResponse<T> success(final SuccessCode successCode) {
-        return new ApiResponse<>(successCode.getCode(), successCode.getMessage(), successCode.getStatus());
+        return new ApiResponse<>(successCode.getStatus(), successCode.getCode(), successCode.getMessage());
     }
 
     public static <T> ApiResponse<T> success(final SuccessCode successCode, final T data) {
-        return new ApiResponse<>(successCode.getCode(), successCode.getMessage(), successCode.getStatus(), data);
+        return new ApiResponse<>(successCode.getStatus(), successCode.getCode(), successCode.getMessage(), data);
     }
 
     public static <T> ApiResponse<T> fail(final ErrorCode errorCode) {
-        return new ApiResponse<>(errorCode.getCode(), errorCode.getMessage(), errorCode.getStatus());
+        return new ApiResponse<>(errorCode.getStatus(), errorCode.getCode(), errorCode.getMessage());
     }
 
     public static <T> ApiResponse<T> fail(final int status, final String code, final String message) {
-        return new ApiResponse<>(code, message, status);
+        return new ApiResponse<>(status, code, message);
     }
 
     public static <T> ApiResponse<T> fail(final int status, final String code, final String message, final T data) {
-        return new ApiResponse<>(code, message, status, data);
+        return new ApiResponse<>(status, code, message, data);
     }
 }
