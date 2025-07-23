f4f9a03dd4977bee9a4bbb9d018db79f6597691b
kgy1008 <gayeon1008@g.skku.edu>
2025-07-24 02:36:09 +0900

    [feat] Set default timezone to Asia/Seoul on application startup

--- edukit-api/src/main/java/com/edukit/api/EdukitApiApplication.java
+++ edukit-api/src/main/java/com/edukit/api/EdukitApiApplication.java
@@ -1,13 +1,23 @@
 package com.edukit.api;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication(scanBasePackages = "com.edukit")
+@SpringBootApplication
 public class EdukitApiApplication {
+
+    private static final String TIMEZONE_KST = "Asia/Seoul";
 
     public static void main(String[] args) {
         SpringApplication.run(EdukitApiApplication.class, args);
     }
 
-} 
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone(TIMEZONE_KST));
+    }
+
+}
