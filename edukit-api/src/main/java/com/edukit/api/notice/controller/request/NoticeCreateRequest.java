e6d1d89634415aa0dc7805fb7acb7c6764c217dc
TaegeunYou <dbxorms2310@naver.com>
2025-07-25 16:47:28 +0900

    [feat] add notice management features

---
+++ edukit-api/src/main/java/com/edukit/api/notice/controller/request/NoticeCreateRequest.java
@@ -0,0 +1,13 @@
+package com.edukit.api.notice.controller.request;
+
+import jakarta.validation.constraints.NotNull;
+
+public record NoticeCreateRequest(
+        @NotNull(message = "카테고리 ID는 필수입니다.")
+        Integer categoryId,
+        @NotNull(message = "제목은 필수입니다.")
+        String title,
+        @NotNull(message = "내용은 필수입니다.")
+        String content
+) {
+}
