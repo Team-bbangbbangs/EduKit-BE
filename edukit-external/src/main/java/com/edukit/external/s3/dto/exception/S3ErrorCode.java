0b76dae385262c3ef3a5c213c606e8bc60649af8
TaegeunYou <dbxorms2310@naver.com>
2025-07-26 02:39:24 +0900

    [fix] error code format in S3ErrorCode enum

--- edukit-external/src/main/java/com/edukit/external/s3/dto/exception/S3ErrorCode.java
+++ edukit-external/src/main/java/com/edukit/external/s3/dto/exception/S3ErrorCode.java
@@ -7,8 +7,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum S3ErrorCode implements ErrorCode {
-    INVALID_FILE_NAME("S3_40001", "잘못된 파일 이름입니다."),
-    INVALID_FILE_EXTENSION("S3_40002", "지원하지 않는 파일 확장자입니다.")
+    INVALID_FILE_NAME("S3-40001", "잘못된 파일 이름입니다."),
+    INVALID_FILE_EXTENSION("S3-40002", "지원하지 않는 파일 확장자입니다.")
     ;
 
     private final String code;
