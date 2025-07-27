5d96c986621415f4e19951aa15be9cfc5cf6b60d
TaegeunYou <dbxorms2310@naver.com>
2025-07-27 17:23:32 +0900

    [refac] change response type to ResponseEntity<EdukitResponse>

--- edukit-api/src/main/java/com/edukit/api/controller/notice/AdminNoticeController.java
+++ edukit-api/src/main/java/com/edukit/api/controller/notice/AdminNoticeController.java
@@ -1,15 +1,15 @@
 package com.edukit.api.controller.notice;
 
-import com.edukit.api.common.ApiResponse;
+import com.edukit.api.common.EdukitResponse;
 import com.edukit.api.controller.notice.request.NoticeCreateRequest;
 import com.edukit.api.controller.notice.request.NoticeUpdateRequest;
-import com.edukit.common.exception.code.CommonSuccessCode;
 import com.edukit.core.notice.enums.NoticeCategory;
 import com.edukit.core.notice.facade.NoticeFacade;
 import com.edukit.core.notice.facade.response.NoticeFileUploadPresignedUrlCreateResponse;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -28,36 +28,36 @@ public class AdminNoticeController {
     private final NoticeFacade noticeFacade;
 
     @PostMapping
-    public ApiResponse<Void> createNotice(
+    public ResponseEntity<EdukitResponse<Void>> createNotice(
             @RequestBody @Valid final NoticeCreateRequest request
     ) {
         NoticeCategory category = NoticeCategory.fromId(request.categoryId());
         noticeFacade.createNotice(category, request.title(), request.content(), request.noticeFileIds());
-        return ApiResponse.success(CommonSuccessCode.OK);
+        return ResponseEntity.ok().body(EdukitResponse.success());
     }
 
     @PatchMapping("/{noticeId}")
-    public ApiResponse<Void> updateNotice(
+    public ResponseEntity<EdukitResponse<Void>> updateNotice(
             @RequestBody @Valid final NoticeUpdateRequest request,
             @PathVariable final long noticeId
     ) {
         NoticeCategory category = NoticeCategory.fromId(request.categoryId());
         noticeFacade.updateNotice(noticeId, category, request.title(), request.content(), request.noticeFileIds());
-        return ApiResponse.success(CommonSuccessCode.OK);
+        return ResponseEntity.ok().body(EdukitResponse.success());
     }
 
     @DeleteMapping("/{noticeId}")
-    public ApiResponse<Void> deleteNotice(
+    public ResponseEntity<EdukitResponse<Void>> deleteNotice(
             @PathVariable final long noticeId
     ) {
         noticeFacade.deleteNotice(noticeId);
-        return ApiResponse.success(CommonSuccessCode.OK);
+        return ResponseEntity.ok().body(EdukitResponse.success());
     }
 
     @GetMapping("/presigned-url")
-    public ApiResponse<NoticeFileUploadPresignedUrlCreateResponse> createFileUploadPresignedUrl(
+    public ResponseEntity<EdukitResponse<NoticeFileUploadPresignedUrlCreateResponse>> createFileUploadPresignedUrl(
             @RequestParam final List<String> filenames
     ) {
-        return ApiResponse.success(CommonSuccessCode.OK, noticeFacade.createFileUploadPresignedUrl(filenames));
+        return ResponseEntity.ok().body(EdukitResponse.success(noticeFacade.createFileUploadPresignedUrl(filenames)));
     }
 }
--- edukit-api/src/main/java/com/edukit/api/controller/notice/NoticeController.java
+++ edukit-api/src/main/java/com/edukit/api/controller/notice/NoticeController.java
@@ -1,13 +1,13 @@
 package com.edukit.api.controller.notice;
 
-import com.edukit.api.common.ApiResponse;
-import com.edukit.common.exception.code.CommonSuccessCode;
+import com.edukit.api.common.EdukitResponse;
 import com.edukit.core.notice.enums.NoticeCategory;
 import com.edukit.core.notice.facade.NoticeFacade;
 import com.edukit.core.notice.facade.response.NoticeGetResponse;
 import com.edukit.core.notice.facade.response.NoticesGetResponse;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,24 +22,18 @@ public class NoticeController {
     private final NoticeFacade noticeFacade;
 
     @GetMapping
-    public ApiResponse<NoticesGetResponse> getNotices(
+    public ResponseEntity<EdukitResponse<NoticesGetResponse>> getNotices(
             @RequestParam(name = "categoryId", required = false) Integer categoryId,
             @RequestParam(name = "page", required = false, defaultValue = "1") @Min(1) int page
     ) {
         NoticeCategory category = NoticeCategory.fromId(categoryId);
-        return ApiResponse.success(
-                CommonSuccessCode.OK,
-                noticeFacade.getNotices(category, page)
-        );
+        return ResponseEntity.ok().body(EdukitResponse.success(noticeFacade.getNotices(category, page)));
     }
 
     @GetMapping("/{noticeId}")
-    public ApiResponse<NoticeGetResponse> getNotice(
+    public ResponseEntity<EdukitResponse<NoticeGetResponse>> getNotice(
             @PathVariable final long noticeId
     ) {
-        return ApiResponse.success(
-                CommonSuccessCode.OK,
-                noticeFacade.getNotice(noticeId)
-        );
+        return ResponseEntity.ok().body(EdukitResponse.success(noticeFacade.getNotice(noticeId)));
     }
 }
