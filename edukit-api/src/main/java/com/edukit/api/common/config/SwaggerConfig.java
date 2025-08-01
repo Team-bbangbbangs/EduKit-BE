2b4ff289dec8e4b2a5f18971c915729da869ae4f
TaegeunYou <dbxorms2310@naver.com>
2025-08-01 11:29:57 +0900

    [fix] remove server configuration from SwaggerConfig

--- edukit-api/src/main/java/com/edukit/api/common/config/SwaggerConfig.java
+++ edukit-api/src/main/java/com/edukit/api/common/config/SwaggerConfig.java
@@ -6,34 +6,22 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
 @Configuration
 @RequiredArgsConstructor
 public class SwaggerConfig {
 
-    private final Environment environment;
-
     private static final String AUTHORIZATION = "Authorization";
-    private static final Map<String, String> PROFILE_SERVER_URL_MAP = Map.of(
-            "local", "http://localhost:8080",
-            "dev", "https://dev-api.edukit.co.kr",
-            "prod", "https://prod-api.edukit.co.kr"
-    );
 
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .info(info())
-                .servers(servers())
                 .addSecurityItem(securityRequirement())
                 .components(components());
     }
@@ -55,17 +43,6 @@ public class SwaggerConfig {
         info.title("Edukit API");
         info.description("Edukit API 명세서");
         return info;
-    }
-
-    private List<Server> servers() {
-        return PROFILE_SERVER_URL_MAP.entrySet().stream()
-                .filter(entry -> environment.matchesProfiles(entry.getKey()))
-                .map(entry -> {
-                    String url = entry.getValue();
-                    String description = "Edukit API " + entry.getKey();
-                    return new Server().url(url).description(description);
-                })
-                .toList();
     }
 
     private SecurityRequirement securityRequirement() {
