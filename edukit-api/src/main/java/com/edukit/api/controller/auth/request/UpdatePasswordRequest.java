f9ceee57c93042cf79ac2378516a4fe04572f11c
TaegeunYou <dbxorms2310@naver.com>
2025-08-01 15:11:09 +0900

    [fix] update password functionality to use member UUID instead of email

--- edukit-api/src/main/java/com/edukit/api/controller/auth/request/UpdatePasswordRequest.java
+++ edukit-api/src/main/java/com/edukit/api/controller/auth/request/UpdatePasswordRequest.java
@@ -1,14 +1,12 @@
 package com.edukit.api.controller.auth.request;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 
 public record UpdatePasswordRequest(
+        @NotNull(message = "유저 uuid를 입력해주세요.")
+        String memberUuid,
         @NotNull(message = "인증 코드는 필수입니다.")
         String verificationCode,
-        @Email(message = "유효한 이메일 형식이 아닙니다.")
-        @NotNull(message = "이메일 주소를 입력해주세요.")
-        String email,
         @NotNull(message = "새 비밀번호를 입력해주세요.")
         String password,
         @NotNull(message = "비밀번호 확인을 입력해주세요.")
