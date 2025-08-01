4c7faebc9d44e4ec8a02cb92fe36dbc34cdb14bf
kgy1008 <gayeon1008@g.skku.edu>
2025-08-01 22:19:22 +0900

    [feat] Optimize batch application by excluding unnecessary dependencies and packages

--- edukit-batch/src/main/java/com/edukit/batch/EdukitBatchApplication.java
+++ edukit-batch/src/main/java/com/edukit/batch/EdukitBatchApplication.java
@@ -3,11 +3,16 @@ package com.edukit.batch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "com.edukit.batch",
+        "com.edukit.core",
+        "com.edukit.common",
+        "com.edukit.external"
+})
 public class EdukitBatchApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EdukitBatchApplication.class, args);
     }
 
-} 
+}
