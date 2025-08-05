1cb465944f4b7577a2cba67fccf894bb65495d33
TaegeunYou <dbxorms2310@naver.com>
2025-08-05 20:39:43 +0900

    [feat] Add ForwardedHeaderFilter bean to WebMvcConfig

--- edukit-api/src/main/java/com/edukit/api/common/config/WebMvcConfig.java
+++ edukit-api/src/main/java/com/edukit/api/common/config/WebMvcConfig.java
@@ -3,7 +3,9 @@ package com.edukit.api.common.config;
 import com.edukit.api.common.resolver.MemberIdArgumentResolver;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,5 +18,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(memberIdArgumentResolver);
+    }
+
+    @Bean
+    public ForwardedHeaderFilter forwardedHeaderFilter() {
+        return new ForwardedHeaderFilter();
     }
 } 
