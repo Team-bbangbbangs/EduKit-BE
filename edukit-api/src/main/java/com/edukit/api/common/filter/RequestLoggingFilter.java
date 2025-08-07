8df1aa6fd8c5111b4b872ccd7ba2da1bed69e822
kgy1008 <gayeon1008@g.skku.edu>
2025-08-08 00:19:13 +0900

    [refac] Remove unused HttpServletResponse import in RequestLoggingFilter for cleaner code

--- edukit-api/src/main/java/com/edukit/api/common/filter/RequestLoggingFilter.java
+++ edukit-api/src/main/java/com/edukit/api/common/filter/RequestLoggingFilter.java
@@ -7,7 +7,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -32,7 +31,6 @@ public class RequestLoggingFilter implements Filter {
             throws IOException, ServletException {
 
         HttpServletRequest httpRequest = (HttpServletRequest) request;
-        HttpServletResponse httpResponse = (HttpServletResponse) response;
 
         try {
             MDC.put("requestUrl", httpRequest.getRequestURI());
