7444f8687392fa72c3a7b116aa2fa0538feb92c7
Jian Kim <gayeon1008@g.skku.edu>
2025-08-08 20:53:39 +0900

    [EDMT-342] develop 브랜치 PR 시 dev 환경 빌드 워크플로우 추가 (#24)
    * [refac] S3Exception handling to remove exception chaining
    
    * [feat] Add CI/CD workflow for API development environment
    
    * [refac] delete MemberBatchService.java
    
    * [refac] remove unused extractKey method from S3ServiceImpl
    
    * [refac] rename CI/CD workflow from api-dev-cd to api-dev-ci

--- edukit-external/src/main/java/com/edukit/external/aws/s3/S3ServiceImpl.java
+++ edukit-external/src/main/java/com/edukit/external/aws/s3/S3ServiceImpl.java
@@ -123,7 +123,7 @@ public class S3ServiceImpl implements FileStorageService {
                     .destinationKey(targetKey));
         } catch (Exception e) {
             log.error("S3 파일 복사 실패: sourceKey={}, targetKey={}", sourceKey, targetKey, e);
-            throw new S3Exception(S3ErrorCode.FILE_COPY_FAILED, e);
+            throw new S3Exception(S3ErrorCode.FILE_COPY_FAILED);
         }
     }
 
@@ -134,19 +134,12 @@ public class S3ServiceImpl implements FileStorageService {
                     .key(key));
         } catch (Exception e) {
             log.error("S3 파일 삭제 실패: key={}", key, e);
-            throw new S3Exception(S3ErrorCode.FILE_DELETE_FAILED, e);
+            throw new S3Exception(S3ErrorCode.FILE_DELETE_FAILED);
         }
     }
 
     private String extractFileName(final String url) {
         int lastSlash = url.lastIndexOf('/');
         return url.substring(lastSlash + 1);
-    }
-
-    private String extractKey(final String url) {
-        if (!url.startsWith(s3Properties.cdnUrl())) {
-            throw new S3Exception(S3ErrorCode.INVALID_FILE_URL);
-        }
-        return url.substring(s3Properties.cdnUrl().length() + 1);
     }
 }
