adfaddb9a2e34b41bb60c408203f60015220e1be
kgy1008 <gayeon1008@g.skku.edu>
2025-08-13 15:21:49 +0900

    [refac] remove deprecated prompt generation methods and comments for cleaner code

--- edukit-api/src/main/java/com/edukit/studentrecord/facade/StudentRecordAIFacade.java
+++ edukit-api/src/main/java/com/edukit/studentrecord/facade/StudentRecordAIFacade.java
@@ -40,24 +40,4 @@ public class StudentRecordAIFacade {
         return StudentRecordCreateResponse.of(response.versionNumber(), response.content(), response.isLast(),
                 response.isFallback());
     }
-
-    /* v1.0.0
-    @Transactional
-    public StudentRecordTaskResponse getPrompt(final long memberId, final long recordId, final int byteCount,
-                                               final String userPrompt) {
-        Member member = memberService.getMemberById(memberId);
-        StudentRecord studentRecord = studentRecordService.getRecordDetail(member.getId(), recordId);
-
-        String requestPrompt = AIPromptGenerator.createPrompt(studentRecord.getStudentRecordType(), byteCount,
-                userPrompt);
-        long taskId = studentRecordService.createAITask(studentRecord, userPrompt);
-        return StudentRecordTaskResponse.of(taskId, requestPrompt);
-    }
-
-    public StudentRecordCreateResponse generateAIStudentRecord(final String prompt) {
-        OpenAIResponse openAIResponse = openAIService.getMultipleChatResponses(prompt);
-        return StudentRecordCreateResponse.of(openAIResponse.description1(), openAIResponse.description2(),
-                openAIResponse.description3());
-    }
-     */
 }
