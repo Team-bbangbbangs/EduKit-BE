214b6fdf9c48a7f6714a0cfa6b8be434641a7317
kgy1008 <gayeon1008@g.skku.edu>
2025-08-15 23:09:40 +0800

    [feat] Add logging for error handling in AIResponseGenerateEvent processing

--- edukit-core/src/main/java/com/edukit/core/common/event/ai/AIEventListener.java
+++ edukit-core/src/main/java/com/edukit/core/common/event/ai/AIEventListener.java
@@ -4,6 +4,7 @@ import com.edukit.core.common.service.AIService;
 import com.edukit.core.common.service.SqsService;
 import com.edukit.core.common.service.response.OpenAIVersionResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,7 @@ import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 import reactor.core.publisher.Flux;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 @ConditionalOnBean({AIService.class, SqsService.class})
@@ -23,6 +25,11 @@ public class AIEventListener {
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleAIResponseGenerateEvent(final AIResponseGenerateEvent event) {
         Flux<OpenAIVersionResponse> response = aiService.getVersionedStreamingResponse(event.requestPrompt());
-
+        response
+                .doOnNext(messageQueueService::sendMessage)
+                .doOnError(error -> {
+                    log.error("Error processing AI response: {}", error.getMessage());
+                })
+                .subscribe();
     }
 }
