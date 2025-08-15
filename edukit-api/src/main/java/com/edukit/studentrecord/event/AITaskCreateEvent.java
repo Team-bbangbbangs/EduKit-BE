647e23fc9ed9a7014473a57822cd9cb2972195b0
kgy1008 <gayeon1008@g.skku.edu>
2025-08-16 00:59:14 +0800

    [feat] Update AITaskCreateEvent to include userPrompt and adjust related event handling

--- edukit-api/src/main/java/com/edukit/studentrecord/event/AITaskCreateEvent.java
+++ edukit-api/src/main/java/com/edukit/studentrecord/event/AITaskCreateEvent.java
@@ -4,14 +4,15 @@ import com.edukit.core.common.event.ai.AIResponseGenerateEvent;
 
 public record AITaskCreateEvent(
         long taskId,
+        String userPrompt,
         String requestPrompt,
         int byteCount,
         long recordId
 
 ) implements AIResponseGenerateEvent {
 
-    public static AITaskCreateEvent of(final long taskId, final String requestPrompt, final int byteCount,
-                                       final long recordId) {
-        return new AITaskCreateEvent(taskId, requestPrompt, byteCount, recordId);
+    public static AITaskCreateEvent of(final long taskId, final String userPrompt, final String requestPrompt,
+                                       final int byteCount, final long recordId) {
+        return new AITaskCreateEvent(taskId, userPrompt, requestPrompt, byteCount, recordId);
     }
 }
