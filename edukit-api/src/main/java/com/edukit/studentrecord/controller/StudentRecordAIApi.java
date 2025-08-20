4751f85e889a17b40860a3f16779e8e2184badf8
kgy1008 <gayeon1008@g.skku.edu>
2025-08-20 12:41:33 +0900

    [feat] Implement StudentRecordAIApi interface and update StudentRecordAIController to implement it

---
+++ edukit-api/src/main/java/com/edukit/studentrecord/controller/StudentRecordAIApi.java
@@ -0,0 +1,7 @@
+package com.edukit.studentrecord.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "생활기록부 생성", description = "생활기록부 생성 API")
+public interface StudentRecordAIApi {
+}
