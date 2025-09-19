1870bfabde7c2781fc18ed70e875028c860ed762
kgy1008 <gayeon1008@g.skku.edu>
2025-09-20 00:06:25 +0900

    [fix] enable AspectJ auto proxy support in EdukitApiApplication

--- edukit-api/src/main/java/com/edukit/EdukitApiApplication.java
+++ edukit-api/src/main/java/com/edukit/EdukitApiApplication.java
@@ -4,7 +4,9 @@ import jakarta.annotation.PostConstruct;
 import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy
 @SpringBootApplication
 public class EdukitApiApplication {
 
