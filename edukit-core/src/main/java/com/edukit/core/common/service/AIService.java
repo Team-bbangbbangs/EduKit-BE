229efa281fe72e3ffe29cb2a121c4f2fa6af9e4a
kgy1008 <gayeon1008@g.skku.edu>
2025-10-21 10:28:10 +0900

    [refac] remove Spring AI dependency from build.gradle

--- edukit-core/src/main/java/com/edukit/core/common/service/AIService.java
+++
@@ -1,9 +0,0 @@
-package com.edukit.core.common.service;
-
-import com.edukit.core.common.service.response.OpenAIVersionResponse;
-import reactor.core.publisher.Flux;
-
-public interface AIService {
-
-    Flux<OpenAIVersionResponse> getVersionedStreamingResponse(String prompt);
-}
